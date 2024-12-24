6e4036239a43bc7c8585fe9cddcb6407cba039fe
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-12-24 10:04:52 -0800

    Remove airbyte

--- docker/src/services/postgres/initdb.d/init-airbyte.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS _ds_airbyte;
-CREATE DATABASE _ds_airbyte;
-
-DROP DATABASE IF EXISTS _ds_airbyte_temporal;
-CREATE DATABASE _ds_airbyte_temporal;
-
-DROP DATABASE IF EXISTS _ds_airbyte_temporal_visibility;
-CREATE DATABASE _ds_airbyte_temporal_visibility;
